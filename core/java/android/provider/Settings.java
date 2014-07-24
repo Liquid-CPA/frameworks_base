733b92dc33ae1c9c7c46af63da2b14a91e45f402
emancebo <emancebo@cyngn.com>
2014-07-24 15:26:14 -0700

    Add system setting for t9 search input locale
    Change-Id: I780b3cca1ad2da396b76464a6dab5cc53f621ed2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3418,10 +3418,10 @@ public final class Settings {
         @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_FIELD)
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
 
-        /**  
+        /**
          * AOKP Custom System Animations
          * @hide
-         */  
+         */
         public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
                 "activity_open",
                 "activity_close",
@@ -3434,7 +3434,7 @@ public final class Settings {
                 "wallpaper_intra_open",
                 "wallpaper_intra_close",
                 "task_open_behind",
-        };  
+        };
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
@@ -3505,7 +3505,15 @@ public final class Settings {
          * 7 == bounce_interpolator
          * @hide
          */
+
         public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+        /**
+         * Locale for secondary overlay on dialer for t9 search input
+         * @hide
+         */
+
+        public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
