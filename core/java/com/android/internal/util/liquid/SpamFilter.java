CyanogenMod/android_frameworks_base

Filter spam notifications
Change-Id: Ibf2377cd7491c7e0e9e9c9657e9e65eef1876ee3

core/java/com/android/internal/util/cm/SpamFilter.java
@@ -1,0 +1,58 @@
+package com.android.internal.util.cm;
+
+import android.app.Notification;
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+public class SpamFilter {
+
+    public static final String AUTHORITY = "com.cyanogenmod.spam";
+    public static final Uri NOTIFICATION_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY)
+            .build();
+
+    public static final class SpamContract {
+
+        public static final class PackageTable {
+            public static final String TABLE_NAME = "packages";
+            public static final String ID = "_id";
+            public static final String PACKAGE_NAME = "package_name";
+        }
+
+        public static final class NotificationTable {
+            public static final String TABLE_NAME = "notifications";
+            public static final String ID = "_id";
+            public static final String PACKAGE_ID = "package_id";
+            public static final String MESSAGE_TEXT = "message_text";
+            public static final String COUNT = "count";
+            public static final String LAST_BLOCKED = "last_blocked";
+            public static final String NORMALIZED_TEXT = "normalized_text";
+        }
+
+    }
+
+    public static String getNormalizedContent(String msg) {
+        return msg.toLowerCase().replaceAll("[^\\p{L}\\p{Nd}]+", "");
+    }
+
+    public static String getNotificationContent(Notification notification) {
+        Bundle extras = notification.extras;
+        String titleExtra = extras.containsKey(Notification.EXTRA_TITLE_BIG)
+                ? Notification.EXTRA_TITLE_BIG : Notification.EXTRA_TITLE;
+        CharSequence notificationTitle = extras.getCharSequence(titleExtra);
+        CharSequence notificationMessage = extras.getCharSequence(Notification.EXTRA_TEXT);
+
+        if (TextUtils.isEmpty(notificationMessage)) {
+            CharSequence[] inboxLines = extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
+            if (inboxLines == null || inboxLines.length == 0) {
+                notificationMessage = "";
+            } else {
+                notificationMessage = TextUtils.join("\n", inboxLines);
+            }
+        }
+        return notificationTitle + "\n" + notificationMessage;
+    }
+}

packages/SystemUI/src/com/android/systemui/cm/SpamOpenHelper.java
@@ -1,0 +1,46 @@
+package com.android.systemui.cm;
+
+import com.android.internal.util.cm.SpamFilter.SpamContract.NotificationTable;
+import com.android.internal.util.cm.SpamFilter.SpamContract.PackageTable;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SpamOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "spam.db";
+    private static final int VERSION = 4;
+    private static final String CREATE_PACKAGES_TABLE =
+            "create table " + PackageTable.TABLE_NAME + "(" +
+             PackageTable.ID + " INTEGER PRIMARY KEY," +
+             PackageTable.PACKAGE_NAME + " TEXT UNIQUE);";
+    private static final String CREATE_NOTIFICATIONS_TABLE =
+            "create table " + NotificationTable.TABLE_NAME + "(" +
+             NotificationTable.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+             NotificationTable.PACKAGE_ID + " INTEGER," +
+             NotificationTable.MESSAGE_TEXT + " STRING," +
+             NotificationTable.LAST_BLOCKED + " INTEGER," +
+             NotificationTable.NORMALIZED_TEXT + " STRING," +
+             NotificationTable.COUNT + " INTEGER DEFAULT 0);";
+
+    private Context mContext;
+
+    public SpamOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PACKAGES_TABLE);
+        db.execSQL(CREATE_NOTIFICATIONS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        mContext.deleteDatabase(DATABASE_NAME);
+        onCreate(db);
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -29,6 +29,8 @@ import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
@@ -41,6 +43,7 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
@@ -83,6 +86,9 @@ import android.widget.TextView;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.statusbar.StatusBarIconList;
+import com.android.internal.util.cm.SpamFilter;
+import com.android.internal.util.cm.SpamFilter.SpamContract.NotificationTable;
+import com.android.internal.util.cm.SpamFilter.SpamContract.PackageTable;
 import com.android.internal.util.NotificationColorUtil;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.R;
@@ -90,6 +96,8 @@ import com.android.systemui.RecentsComponent;
 import com.android.systemui.SearchPanelView;
 import com.android.systemui.SwipeHelper;
 import com.android.systemui.SystemUI;
+import com.android.systemui.cm.SpamMessageProvider;
+import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.phone.NavigationBarView;
@@ -141,6 +149,12 @@ public abstract class BaseStatusBar extends SystemUI implements
             "com.android.systemui.statusbar.banner_action_cancel";
     private static final String BANNER_ACTION_SETUP =
             "com.android.systemui.statusbar.banner_action_setup";
+
+    private static final Uri SPAM_MESSAGE_URI = new Uri.Builder()
+           .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(SpamMessageProvider.AUTHORITY)
+            .appendPath("message")
+            .build();
 
     protected CommandQueue mCommandQueue;
     protected IStatusBarService mBarService;
@@ -805,11 +819,25 @@ public abstract class BaseStatusBar extends SystemUI implements
         final View settingsButton = guts.findViewById(R.id.notification_inspect_item);
         final View appSettingsButton
                 = guts.findViewById(R.id.notification_inspect_app_provided_settings);
+        final View filterButton = guts.findViewById(R.id.notification_inspect_filter_notification);
         if (appUid >= 0) {
             final int appUidF = appUid;
             settingsButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     startAppNotificationSettingsActivity(pkg, appUidF);
+                }
+            });
+
+            filterButton.setVisibility(View.VISIBLE);
+            filterButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    ContentValues values = new ContentValues();
+                    String message = SpamFilter.getNotificationContent(
+                    sbn.getNotification());
+                    values.put(NotificationTable.MESSAGE_TEXT, message);
+                    values.put(PackageTable.PACKAGE_NAME, pkg);
+                    mContext.getContentResolver().insert(SPAM_MESSAGE_URI, values);
+                    removeNotification(sbn.getKey(), null);
                 }
             });
 
@@ -841,6 +869,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         } else {
             settingsButton.setVisibility(View.GONE);
             appSettingsButton.setVisibility(View.GONE);
+            filterButton.setVisibility(View.GONE);
         }
 
     }

Liquid-CPA/frameworks_base

Conflicts:
	packages/SystemUI/res/layout/notification_guts.xml
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

core/java/com/android/internal/util/liquid/SpamFilter.java
@@ -1,0 +1,58 @@
+package com.android.internal.util.liquid;
+
+import android.app.Notification;
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+public class SpamFilter {
+
+    public static final String AUTHORITY = "com.liquidsmooth.spam";
+    public static final Uri NOTIFICATION_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(AUTHORITY)
+            .build();
+
+    public static final class SpamContract {
+
+        public static final class PackageTable {
+            public static final String TABLE_NAME = "packages";
+            public static final String ID = "_id";
+            public static final String PACKAGE_NAME = "package_name";
+        }
+
+        public static final class NotificationTable {
+            public static final String TABLE_NAME = "notifications";
+            public static final String ID = "_id";
+            public static final String PACKAGE_ID = "package_id";
+            public static final String MESSAGE_TEXT = "message_text";
+            public static final String COUNT = "count";
+            public static final String LAST_BLOCKED = "last_blocked";
+            public static final String NORMALIZED_TEXT = "normalized_text";
+        }
+
+    }
+
+    public static String getNormalizedContent(String msg) {
+        return msg.toLowerCase().replaceAll("[^\\p{L}\\p{Nd}]+", "");
+    }
+
+    public static String getNotificationContent(Notification notification) {
+        Bundle extras = notification.extras;
+        String titleExtra = extras.containsKey(Notification.EXTRA_TITLE_BIG)
+                ? Notification.EXTRA_TITLE_BIG : Notification.EXTRA_TITLE;
+        CharSequence notificationTitle = extras.getCharSequence(titleExtra);
+        CharSequence notificationMessage = extras.getCharSequence(Notification.EXTRA_TEXT);
+
+        if (TextUtils.isEmpty(notificationMessage)) {
+            CharSequence[] inboxLines = extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
+            if (inboxLines == null || inboxLines.length == 0) {
+                notificationMessage = "";
+            } else {
+                notificationMessage = TextUtils.join("\n", inboxLines);
+            }
+        }
+        return notificationTitle + "\n" + notificationMessage;
+    }
+}

packages/SystemUI/src/com/android/systemui/liquid/SpamOpenHelper.java
@@ -1,0 +1,46 @@
+package com.android.systemui.liquid;
+
+import com.android.internal.util.liquid.SpamFilter.SpamContract.NotificationTable;
+import com.android.internal.util.liuiqd.SpamFilter.SpamContract.PackageTable;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SpamOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "spam.db";
+    private static final int VERSION = 4;
+    private static final String CREATE_PACKAGES_TABLE =
+            "create table " + PackageTable.TABLE_NAME + "(" +
+             PackageTable.ID + " INTEGER PRIMARY KEY," +
+             PackageTable.PACKAGE_NAME + " TEXT UNIQUE);";
+    private static final String CREATE_NOTIFICATIONS_TABLE =
+            "create table " + NotificationTable.TABLE_NAME + "(" +
+             NotificationTable.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+             NotificationTable.PACKAGE_ID + " INTEGER," +
+             NotificationTable.MESSAGE_TEXT + " STRING," +
+             NotificationTable.LAST_BLOCKED + " INTEGER," +
+             NotificationTable.NORMALIZED_TEXT + " STRING," +
+             NotificationTable.COUNT + " INTEGER DEFAULT 0);";
+
+    private Context mContext;
+
+    public SpamOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PACKAGES_TABLE);
+        db.execSQL(CREATE_NOTIFICATIONS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        mContext.deleteDatabase(DATABASE_NAME);
+        onCreate(db);
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -29,6 +29,8 @@ import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
@@ -42,6 +44,7 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Handler;
@@ -86,6 +89,9 @@ import android.widget.Toast;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.statusbar.StatusBarIconList;
+import com.android.internal.util.liquid.SpamFilter;
+import com.android.internal.util.liquid.SpamFilter.SpamContract.NotificationTable;
+import com.android.internal.util.liquid.SpamFilter.SpamContract.PackageTable;
 import com.android.internal.util.NotificationColorUtil;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.systemui.R;
@@ -93,6 +99,8 @@ import com.android.systemui.RecentsComponent;
 import com.android.systemui.SearchPanelView;
 import com.android.systemui.SwipeHelper;
 import com.android.systemui.SystemUI;
+import com.android.systemui.liquid.SpamMessageProvider;
+import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.phone.NavigationBarView;
@@ -143,6 +151,12 @@ public abstract class BaseStatusBar extends SystemUI implements
             "com.android.systemui.statusbar.banner_action_cancel";
     private static final String BANNER_ACTION_SETUP =
             "com.android.systemui.statusbar.banner_action_setup";
+
+    private static final Uri SPAM_MESSAGE_URI = new Uri.Builder()
+           .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(SpamMessageProvider.AUTHORITY)
+            .appendPath("message")
+            .build();
 
     protected CommandQueue mCommandQueue;
     protected IStatusBarService mBarService;
@@ -812,11 +826,25 @@ public abstract class BaseStatusBar extends SystemUI implements
                 = guts.findViewById(R.id.notification_inspect_app_provided_settings);
         final View headsUpButton
                 = guts.findViewById(R.id.notification_inspect_heads_up);
+        final View filterButton = guts.findViewById(R.id.notification_inspect_filter_notification);
         if (appUid >= 0) {
             final int appUidF = appUid;
             settingsButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     startAppNotificationSettingsActivity(pkg, appUidF);
+                }
+            });
+
+            filterButton.setVisibility(View.VISIBLE);
+            filterButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    ContentValues values = new ContentValues();
+                    String message = SpamFilter.getNotificationContent(
+                    sbn.getNotification());
+                    values.put(NotificationTable.MESSAGE_TEXT, message);
+                    values.put(PackageTable.PACKAGE_NAME, pkg);
+                    mContext.getContentResolver().insert(SPAM_MESSAGE_URI, values);
+                    removeNotification(sbn.getKey(), null);
                 }
             });
 
@@ -875,6 +903,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         } else {
             settingsButton.setVisibility(View.GONE);
             appSettingsButton.setVisibility(View.GONE);
+            filterButton.setVisibility(View.GONE);
             headsUpButton.setVisibility(View.GONE);
         }
 